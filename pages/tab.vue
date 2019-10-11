ec1206fcd4691f14566303cc19ba819602f65eb2
fuhao1517 <53571596+fuhao1517@users.noreply.github.com>
2019-10-11 19:17:09 +0800

    公共布局和首页轮播图

--- pages/tab.vue
+++ pages/tab.vue
@@ -1,9 +1,11 @@
 <template>
   <div class="btns">
-    <span 
-    v-for="(item,index) in ['新闻','娱乐','体育']" 
-    key="index">{{item}}</span>
-  
+    <span
+      v-for="(item,index) in ['新闻','娱乐','体育']"
+      :key="index"
+      :class="{active:index===current}"
+      @click="handleClick(index)"
+    >{{item}}</span>
 
     <div v-if="current===0">
       <p>无锡上跨桥侧翻疑似涉事货车公司曝光:1年涉6起事故</p>
@@ -30,11 +32,16 @@
 
 <script>
 export default {
-    data(){
-        return{
-            current:0
-        }
+  data() {
+    return {
+      current: 0
+    };
+  },
+  methods: {
+    handleClick(index) {
+      this.current = index;
     }
+  }
 };
 </script>
 
@@ -44,14 +51,12 @@ export default {
   span {
     border: 1px solid #eee;
     padding: 5px;
-    
   }
-  div{
-      margin-top: 50px;
+  div {
+    margin-top: 50px;
   }
-  .active{
-      color:red;
-      background:red;
+  .active {
+    background: red;
   }
 }
 </style>
