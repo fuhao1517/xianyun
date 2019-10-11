410890f22d8cdd16f8e0c3fc743675aad1d32e24
fuhao1517 <53571596+fuhao1517@users.noreply.github.com>
2019-10-11 16:30:17 +0800

    项目基本配置完成

---
+++ pages/tab.vue
@@ -0,0 +1,57 @@
+<template>
+  <div class="btns">
+    <span 
+    v-for="(item,index) in ['新闻','娱乐','体育']" 
+    key="index">{{item}}</span>
+  
+
+    <div v-if="current===0">
+      <p>无锡上跨桥侧翻疑似涉事货车公司曝光:1年涉6起事故</p>
+      <p>无锡上跨桥侧翻疑似涉事货车公司曝光:1年涉6起事故</p>
+      <p>无锡上跨桥侧翻疑似涉事货车公司曝光:1年涉6起事故</p>
+      <p>无锡上跨桥侧翻疑似涉事货车公司曝光:1年涉6起事故</p>
+    </div>
+
+    <div v-if="current===1">
+      <p>明星外卖名搞笑：热巴叫老王大张伟叫梁朝伟</p>
+      <p>明星外卖名搞笑：热巴叫老王大张伟叫梁朝伟</p>
+      <p>明星外卖名搞笑：热巴叫老王大张伟叫梁朝伟</p>
+      <p>明星外卖名搞笑：热巴叫老王大张伟叫梁朝伟</p>
+    </div>
+
+    <div v-if="current===2">
+      <p>全民皆兵！活塞全队7人得分上双力克独行侠</p>
+      <p>全民皆兵！活塞全队7人得分上双力克独行侠</p>
+      <p>全民皆兵！活塞全队7人得分上双力克独行侠</p>
+      <p>全民皆兵！活塞全队7人得分上双力克独行侠</p>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+    data(){
+        return{
+            current:0
+        }
+    }
+};
+</script>
+
+<style scoped lang="less">
+.btns {
+  padding: 50px;
+  span {
+    border: 1px solid #eee;
+    padding: 5px;
+    
+  }
+  div{
+      margin-top: 50px;
+  }
+  .active{
+      color:red;
+      background:red;
+  }
+}
+</style>
