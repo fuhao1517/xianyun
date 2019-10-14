d70f72e7d8b2160ffb5edf044e2dc40fa757c2bd
fuhao1517 <53571596+fuhao1517@users.noreply.github.com>
2019-10-14 17:13:14 +0800

    拦截axios错误

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,26 +2,21 @@ import {
   Message
 } from "element-ui";
 
-export default function ({
-  $axios,
-  redirect
-}) {
-  $axios.onError(err => {
+// nuxt插件的固定写法
+// context包含nuxt下所有的方法，固定有的参数
+export default (context) => {
+  // 拦截错误的响应信息， 跟以前项目的中main.js中拦截器不一样.
+  // main.js中拦截器: 拦截所有的请求响应
+  // 当前的拦截只拦截错误,如果请求错误就会执行onError中的函数
+  context.$axios.onError(res => {
+    /* res是错误的对象, Error的对象可以通过response获取详细信息 */
     const {
-      statusCode,
-      message
-    } = err.response.data;
-
-    // 还未使用
-    // if(statusCode === 401 || statusCode === 403){
-    //     Message.warning({message: "请先登录"});
-    //     redirect("/user/login");
-    // }
-
+      message,
+      statusCode
+    } = res.response.data;
     if (statusCode === 400) {
-      Message.warning({
-        message
-      });
+      Message.erroe(message)
     }
   })
+
 }
