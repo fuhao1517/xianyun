1f575a6d9f62f429d5948b73f7839e6d42226619
fuhao1517 <53571596+fuhao1517@users.noreply.github.com>
2019-10-15 19:18:39 +0800

    使用computed实现dataList

--- pages/air/flights.vue
+++ pages/air/flights.vue
@@ -39,14 +39,25 @@ export default {
   data() {
     return {
       // 请求机票列表返回的总数据，包含了flights,info, options,total
-      flightsData: {},
-      /* 从filght总列表中切割出来的数组列表 */
-      dataList: [],
+      flightsData: {
+        /* 初始值 */
+        flights: []
+      },
       /*当前的页数 */
       pageIndex: 1,
       /* 当前页面的条数 */
       pageSize: 5
     };
+  },
+  computed: {
+    /* 从flights总列表数据中切割出来数组列表 */
+    dataList() {
+      const arr = this.flightsData.flights.slice(
+        (this.pageIndex - 1) * this.pageSize,
+        this.pageIndex * this.pageSize
+      );
+      return arr;
+    }
   },
   components: {
     FlightsListHead,
@@ -57,21 +68,21 @@ export default {
     handleSizeChange(val) {
       /* 修改当前的条数 */
       this.pageSize = val;
-      /* 重新回到第一页 */
-      this.dataList = this.flightsData.flights.slice(0, this.pageSize);
+      //   /* 重新回到第一页 */
+      //   this.dataList = this.flightsData.flights.slice(0, this.pageSize);
     },
     // 页数切换时候触发, val是当前的页数
     handleCurrentChange(val) {
       /* 修改当前的页数 */
       this.pageIndex = val;
-      /* 修改机票列表 */
-      // 0, 5
-      // 5, 10
-      // 10, 15
-      this.dataList = this.flightsData.flights.slice(
-        (this.pageIndex - 1) * this.pageSize,
-        this.pageIndex * this.pageSize
-      );
+      //   /* 修改机票列表 */
+      //   // 0, 5
+      //   // 5, 10
+      //   // 10, 15
+      //   this.dataList = this.flightsData.flights.slice(
+      //     (this.pageIndex - 1) * this.pageSize,
+      //     this.pageIndex * this.pageSize
+      //   );
     }
   },
   mounted() {
@@ -82,8 +93,8 @@ export default {
     }).then(res => {
       // 保存到机票的总数据
       this.flightsData = res.data;
-      /* 第一页的数据 */
-      this.dataList = this.flightsData.flights.slice(0, this.pageSize);
+      //   /* 第一页的数据 */
+      //   this.dataList = this.flightsData.flights.slice(0, this.pageSize);
     });
   }
 };
