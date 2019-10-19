1b1e669b419383151ad32121748311675e2cbfa3
fuhao1517 <53571596+fuhao1517@users.noreply.github.com>
2019-10-19 17:13:02 +0800

    插槽的使用

--- pages/hotel/index.vue
+++ pages/hotel/index.vue
@@ -1,15 +1,34 @@
 <template>
   <div>
-      酒店模块首页
+    酒店模块首页
+    <!-- <script
+      type="text/javascript"
+      src="https://webapi.amap.com/maps?v=1.4.15&key=fd675b2c65d8924ae177ad3e95a75406"
+    ></script>-->
+    <script type="text/javascript" src="https://webapi.amap.com/maps?v=1.4.15&key=fd675b2c65d8924ae177ad3e95a75406"></script>
+    <div id="container"></div>
   </div>
 </template>
 
 <script>
 export default {
-
-}
+  mounted() {
+    window.onLoad = function() {
+      var map = new AMap.Map("container");
+    };
+    var url =
+      "https://webapi.amap.com/maps?v=1.4.15&key=fd675b2c65d8924ae177ad3e95a75406&callback=onLoad";
+    var jsapi = document.createElement("script");
+    jsapi.charset = "utf-8";
+    jsapi.src = url;
+    document.head.appendChild(jsapi);
+  }
+};
 </script>
 
-<style>
-
+<style scoped lang="less">
+#container {
+  width: 300px;
+  height: 180px;
+}
 </style>
---
+++ pages/slot.vue
@@ -0,0 +1,20 @@
+<template>
+  <div style="padding:50px;">
+    <!-- 调用按钮组件 -->
+    <SlotButton>
+      <span style="color:red">123</span>
+    </SlotButton>
+  </div>
+</template>
+
+<script>
+import SlotButton from "@/components/slotButton";
+export default {
+  components: {
+    SlotButton
+  }
+};
+</script>
+
+<style>
+</style>
