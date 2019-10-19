c9d6e36362fef6dad6d07f0b98b816a4f5590845
fuhao1517 <53571596+fuhao1517@users.noreply.github.com>
2019-10-19 19:22:18 +0800

    组件的递归

---
+++ components/post/menuItem.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="digui">
+    <div v-for="(item,index) in data" :key="index">
+      {{item.name}}
+      <!-- 自己调用自己 -->
+      <menuItem :data="item.children" v-if="item.children" />
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "menuItem",
+  props: ["data"]
+};
+</script>
+
+<style scoped lang="less">
+.digui div {
+  padding: 10px;
+}
+</style>
--- pages/post/index.vue
+++ pages/post/index.vue
@@ -1,15 +1,53 @@
 <template>
   <div>
-      旅游攻略首页
+    <div>旅游攻略首页</div>
+    <h3>组件的递归</h3>
+    <!-- 组件接收的是数组 -->
+    <MenuItem :data="list" />
   </div>
 </template>
 
 <script>
+import MenuItem from "@/components/post/menuItem";
 export default {
-
-}
+  components: {
+    MenuItem
+  },
+  data() {
+    return {
+      list: [
+        {
+          name: "服装",
+          children: [
+            {
+              name: "男装",
+              children: [
+                { name: "正装" },
+                { name: "休闲装" },
+                { name: "运动装" }
+              ]
+            },
+            {
+              name: "女装",
+              children: [
+                { name: "正装" },
+                { name: "休闲装" },
+                { name: "运动装" },
+                { name: "xxx" }
+              ]
+            },
+            { name: "童装" }
+          ]
+        },
+        {
+          name: "电器",
+          children: [{ name: "电视" }, { name: "空调" }, { name: "冰箱" }]
+        }
+      ]
+    };
+  }
+};
 </script>
 
 <style>
-
 </style>
